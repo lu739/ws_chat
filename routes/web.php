fbe30800a9575db6a927a43b1f8d90778e3ddea3
lu739 <kroshkalu739@gmail.com>
2024-10-12 22:13:47 +0300

    create base

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@ Route::get('/', function () {
     ]);
 });
 
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/chats', [\App\Http\Controllers\ChatController::class, 'index'])
+        ->name('chats.index');
+    Route::get('/chats/{chat}', [\App\Http\Controllers\ChatController::class, 'show'])
+        ->name('chats.show');
+    Route::post('/chats', [\App\Http\Controllers\ChatController::class, 'store'])
+        ->name('chats.store');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
