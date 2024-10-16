74fc890d5e23c30c1a9858f701fbd66bbdd1c2d9
lu739 <kroshkalu739@gmail.com>
2024-10-16 19:45:49 +0300

    add jobs

--- app/Events/StoreMessageStatusEvent.php
+++ app/Events/StoreMessageStatusEvent.php
@@ -6,12 +6,12 @@ namespace App\Events;
 use App\Models\Chat;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 
-class StoreMessageStatusEvent implements ShouldBroadcast
+class StoreMessageStatusEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
